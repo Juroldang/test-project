bf953043e889988e41fd079ea7f31f64d868a8a3
Juroldang <juroldang@gmail.com>
2023-03-26 11:06:35 -0500

    internacionalizacion

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] arg) {
-        System.out.println("Hola mundo");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
